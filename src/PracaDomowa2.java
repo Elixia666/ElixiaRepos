8e38c7bfe4adaa93d01367e3d4cdfe1e1a038db3
Elixia <skirsza@wp.pl>
2022-07-23 21:29:44 +0200

    Pierwsze zadanie domowe 2 v2

--- src/PracaDomowa2.java
+++ src/PracaDomowa2.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class PracaDomowa2 {
     public static void main(String[] args) {
-
+        Scanner read = new Scanner(System.in);
+        int age = 0;
+        while(age < 30) {
+            System.out.println("Podaj swoj wiek:");
+            age = read.nextInt();
+            if (age >= 18) {
+                System.out.println("Dziekujemy za zakupy");
+            } else {
+                System.out.println("Nie mozesz kupic alkoholu");
+            }
+        }
     }
 }
