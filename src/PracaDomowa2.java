a901537f1dc95dcbd7b6a1bf8b00d84f4b2775c8
Elixia <skirsza@wp.pl>
2022-07-23 21:57:52 +0200

    Pierwsze zadanie domowe 3 v1

--- src/PracaDomowa2.java
+++ src/PracaDomowa2.java
@@ -7,7 +7,9 @@ public class PracaDomowa2 {
         while(age < 30) {
             System.out.println("Podaj swoj wiek:");
             age = read.nextInt();
-            if (age >= 18) {
+            if (age <0){
+                System.out.println("Podany wiek jest nieprawidlowy");
+            }else if (age >= 18) {
                 System.out.println("Dziekujemy za zakupy");
             } else {
                 System.out.println("Nie mozesz kupic alkoholu");
