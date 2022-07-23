2b38a5324ff3b794b89fe55824caed7fc3a83bbf
Elixia <skirsza@wp.pl>
2022-07-23 20:59:26 +0200

    Pierwsze zadanie domowe v1

--- src/PracaDomowa1.java
+++ src/PracaDomowa1.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class PracaDomowa1 {
+    public static void main(String[] args) {
+
+        System.out.println("Podaj pierwsza liczbe:");
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println("Podaj druga liczbe:");
+        int y = scan.nextInt();
+
+        int suma = x+y;
+        int roznica = x-y;
+        int iloczyn = x*y;
+        int iloraz = x/y;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Roznica: "+roznica);
+        System.out.println("Iloczyn: "+iloczyn);
+        System.out.println("Iloraz: "+iloraz);
+    }
 }
