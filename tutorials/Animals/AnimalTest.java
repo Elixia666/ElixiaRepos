28a135f99355a8b399295921a3185ed237d43ed2
Unholy <jagalzor@gmail.com>
2022-07-25 14:46:54 +0200

    wyjatki part 1

---
+++ tutorials/Animals/AnimalTest.java
@@ -0,0 +1,10 @@
+package Animals;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        Animal doggy = new Dog();
+        doggy.sound();
+        Animal kitty = new Cat();
+        kitty.sound();
+    }
+}
---
+++ tutorials/ExceptionsExample.java
@@ -0,0 +1,13 @@
+public class ExceptionsExample {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+
+        numbers[0] = 1;
+        numbers[1] = 3;
+        numbers[2] = 5;
+
+        for(int i=0;i<= numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
+    }
+}
