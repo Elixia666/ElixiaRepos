72409315df381073188244f8f6b43e4175460c88
Unholy <jagalzor@gmail.com>
2022-07-25 13:40:04 +0200

    polimorfizm

--- tutorials/Drivers/WebDriverTest.java
+++ tutorials/Drivers/WebDriverTest.java
@@ -2,12 +2,22 @@ package Drivers;
 
 public class WebDriverTest {
     public static void main(String[] args){
-        ChromeDriver chrome = new ChromeDriver();
+        WebDriver driver = getDriver("firefox");
+        driver.get();
+
+     /*   ChromeDriver chrome = new ChromeDriver();
         FirefoxDriver mozilla = new FirefoxDriver();
         chrome.get();
         chrome.findElementBy();
         mozilla.get();
         mozilla.findElementBy();
+*/
+    }
+
+    private static WebDriver getDriver(String name) {
+        if(name.equals("chrome")) return new ChromeDriver();
+        else if (name.equals("firefox"))  return new FirefoxDriver();
+        return null;
 
     }
 }
